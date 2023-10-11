0311713d9c494cbe1439e4c131d82e9b7b9fdeef
stella <stellamaia97@hotmail.com>
2023-10-11 16:44:46 -0300

    Passando Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 //a função sempre começa com o nome do arquivo, exemplo App
 //dentro do nosso componente, temos um retorno
-function App(){
 
+import Titulo from "./Titulo"
+function App() {
+    return (
+        <div>
+            <Titulo cor="blue" nome="Maia" paragrafo={true}/>
+          {/* passando prop para o component titulo */}
+
+            <Titulo/>
+            <Titulo/>
+            <Titulo/>
+        </div>
+    )
+    
 }
+export default App
+//exporta ele para a main 
---
+++ src/Titulo.jsx
@@ -0,0 +1,13 @@
+function Titulo({ nome, paragrafo, cor }) {
+    //passando prop para o component titulo 
+    return (
+        <div>
+            <h1 style={{ color: cor }}> Oi eu sou tiago {nome ? nome : "vitorino"}</h1>
+            {paragrafo
+                ? <p>Loren Ipsum is simply dummy text</p>
+                :<p>Nada</p>
+            }
+        </div>
+    )
+}
+export default Titulo
