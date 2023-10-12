87fe928387a8645df1ea7b105452e15f5aec43e9
stella <stellamaia97@hotmail.com>
2023-10-12 00:44:36 -0300

    Adiciona Rotas de Navegação em App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 //a função sempre começa com o nome do arquivo, exemplo App
 //dentro do nosso componente, temos um retorno
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Title from "./Title"
 import StateTitle from "./StateTitle"
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
+import Contato from "./pages/Contato"
 function App() {
     return (
         <div>
@@ -12,6 +15,18 @@ function App() {
             <Title />
             <StateTitle />
             <StateTitle />
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}>
+                    </Route>
+                    <Route path="/sobre" element={<Sobre />}>
+                    </Route>
+                    <Route path="/contato" element={<Contato />}>
+                    </Route>
+
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 
