9bcf60c40f71cf8c788eed1cd09378b4e72574cd
stella <stellamaia97@hotmail.com>
2023-10-13 16:00:36 -0300

    Importação de componentes relacionados

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,10 +1,23 @@
-import Menu from "./Menu"
 
-function Sobre(){
-    return(
+//a função sempre começa com o nome do arquivo, exemplo App
+//dentro do nosso componente, temos um retornoimport { BrowserRouter, Routes, Route } from "react-router-dom"
+import Title from "../Title"
+import StateTitle from "../StateTitle"
+import Menu from "./Menu"
+function Sobre() {
+    return (
         <div>
-            <h1>Sobre</h1>
-            <Menu/>
+            <Menu />
+            <h1>Sobreeee</h1>
+
+            <Title cor="blue" nome="Maia" paragrafo={true} />
+            {/* passando prop para o component Title */}
+
+            <Title />
+            <StateTitle />
+            <StateTitle />
+
+
         </div>
     )
 }
