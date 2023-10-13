3f9d388f2ad08056459c6c72a3f06266c3a535c1
stella <stellamaia97@hotmail.com>
2023-10-13 16:01:07 -0300

    Adiciona configuração de rotas e componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 //a função sempre começa com o nome do arquivo, exemplo App
 //dentro do nosso componente, temos um retorno
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Title from "./Title"
-import StateTitle from "./StateTitle"
+
 import Home from "./pages/Home"
 import Sobre from "./pages/Sobre"
 import Contato from "./pages/Contato"
+
 function App() {
     return (
         <div>
-            <Title cor="blue" nome="Maia" paragrafo={true} />
-            {/* passando prop para o component Title */}
-
-            <Title />
-            <StateTitle />
-            <StateTitle />
-
+       
+ 
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />}>
