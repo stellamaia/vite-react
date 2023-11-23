9ed93b2719dce47384d661972eb612a2c7dd5c09
Stella Maia <stellamaia97@hotmail.com>
2023-11-23 10:26:22 -0300

    Tradução do código

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,72 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './TodoList.css';
 import List from './assets/List.png';
 function TodoList() {
-    const [lista, setLista] = useState([]);
-    const [novoItem, setNovoItem] = useState("");
+    const listStorage = localStorage.getItem('List');
+    const [list, setList] = useState(listStorage ? JSON.parse(listStorage) : []);
+    const [newItem, setNewItem] = useState("");
 
-    function adicionaItem(form) {
-        form.preventDefault();
-        if (!novoItem) {
+    useEffect(() => {
+        localStorage.setItem('List', JSON.stringify(list));
+    }, [list]);
+
+    function addItem(event) {
+        event.preventDefault();
+        if (!newItem) {
             return;
         }
-        setLista([...lista, { text: novoItem, isCompleto: false }]);
-        setNovoItem("");
-        document.getElementById("input-entrada").focus()
+        setList([...list, { text: newItem, isCompleted: false }]);
+        setNewItem("");
+        document.getElementById("input-entry").focus();
     }
-    function clicou(index) {
-        const listaAux = [...lista];
-        listaAux[index].isCompleted = !listaAux[index].isCompleted;
-        setLista(listaAux);
-    }
-    function deleta(index) {
-        const listaAux = [...lista];
-        listaAux.splice(index, 1);
-        setLista(listaAux);//pegar a lista e alimentar a nova
 
+    function handleClick(index) {
+        const updatedList = [...list];
+        updatedList[index].isCompleted = !updatedList[index].isCompleted;
+        setList(updatedList);
     }
-    function deleteAll() {
-        setLista([]);
+
+    function deleteItem(index) {
+        const updatedList = [...list];
+        updatedList.splice(index, 1);
+        setList(updatedList);
     }
+
+    function deleteAllItems() {
+        setList([]);
+    }
+
     return (
         <div>
-            <h1>Lista de Tarefa</h1>
-            <form onSubmit={adicionaItem}>
+            <h1>Task List</h1>
+            <form onSubmit={addItem}>
                 <input
-                    id="input-entrada"
+                    id="input-entry"
                     type="text"
-                    value={novoItem}
-                    onChange={(e) => setNovoItem(e.target.value)}
-                    // controlar a entrada de dados em um elemento de formulário (como um campo de texto)
-                    // e atualizar o estado do componente com o valor inserido pelo usuário.
-                    placeholder="Adicione uma tarefa" />
+                    value={newItem}
+                    onChange={(e) => setNewItem(e.target.value)}
+                    placeholder="Add a task" />
                 <button className="add" type="submit">Add</button>
             </form>
-            <div className="listaTarefas">
-                <div style={{ textAlign: 'Center' }}>
-                    {
-                        lista.length < 1
-                            ?
-                            <img className="list" src={List} />
-                            :
-                            lista.map((item, index) => (
-                                <div
-                                    key={index}
-                                    className={item.isCompleted ? "item completo" : "item"}>
-                                    <span onClick={() => { clicou(index) }}>{item.text}</span>
-                                    <button className="del" onClick={() => { deleta(index) }}>Deletar</button>
-
-                                </div>
-                            ))
-
+            <div className="taskList">
+                <div style={{ textAlign: 'center' }}>
+                    {list.length < 1 ?
+                        <img className="list" src={List} />
+                        :
+                        list.map((item, index) => (
+                            <div
+                                key={index}
+                                className={item.isCompleted ? "item complete" : "item"}>
+                                <span onClick={() => handleClick(index)}>{item.text}</span>
+                                <button className="del" onClick={() => deleteItem(index)}>Delete</button>
+                            </div>
+                        ))
                     }
-                    <div className="content-deleteAll">
-                        <button className="deleteAll" onClick={() => { deleteAll([]) }}>Deletar Todas</button>
-                    </div>                </div>
-
-
+                    {list.length > 0 &&
+                        <div className="content-deleteAll">
+                            <button className="deleteAll" onClick={() => deleteAllItems([])}>Delete All</button>
+                        </div>
+                    }
+                </div>
             </div>
         </div>
-    )
+    );
 }
-export default TodoList
+
+export default TodoList;
